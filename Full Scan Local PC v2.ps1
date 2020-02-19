454362c38c02c6518a920aac6a55f2767ead8641
mslijkhuis <mslijkhuis@users.noreply.github.com>
2020-02-19 15:29:10 +0100

    Update Full Scan Local PC v2.ps1
    Cleanup of Layout

--- Full Scan Local PC v2.ps1
+++ Full Scan Local PC v2.ps1
@@ -1,4 +1,4 @@
-﻿# -----------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------
 # Written by: Ruerd Jan van der Wal
 # Date: 19-02-2020
 # Platform: Tested on Windows 7 and Windows 10
@@ -17,34 +17,33 @@ $leeftijdBestandDagen = 75
 # Powershell can not count the value of 1. Therefor, a small if the make sure the numbers match.
 if (($drives.Count) -le 1) {
     $driveCount = 1
-    } else {
+} else {
     $driveCount = ($drives.count)
-    }
+}
 
 Add-Content -Path $logFile -value "----------------------------------------------------------------------------------"
 Add-Content -Path $logFile -Value ((Get-Date).ToString() + " Scan gestart op " + $computerName)
 Add-Content -Path $logFile -Value ("Er zijn " + $driveCount + " lokale drives gevonden op " + $computerName)
 
-foreach ($drive in $drives) {
-    
+foreach ($drive in $drives) {  
     $driveLetter = $drive.DeviceID+"\"
     $scanFile = $computerName + "_" + $drive.DeviceID.Substring(0,1) + "drive" + $scanFileExtension
 
     if (Test-Path ($outFilePath+$scanFile)) {
-
         $leeftijdBestand = Get-ChildItem ($outFilePath + $scanFile) | New-TimeSpan
 
-            if ($leeftijdBestand.TotalSeconds -gt $leeftijdBestandDagen) {
-		        Remove-Item -Path ($outFilePath + $scanFile) -Force
-                Add-Content -Path $logFile -Value ("Logboek van " + $computerName + " " + $driveLetter + " is verwijderd wegens " + $leeftijdBestandDagen + " dagen oud.")
-                Get-ChildItem -Path $driveLetter -Recurse -Force -ErrorAction SilentlyContinue | Where {$_.Length -gt $fileSize} | Select-Object Mode,Name,CreationTime,DirectoryName,Length | Export-Csv -Path ($outFilePath + $scanFile) -NoTypeInformation
-                Add-Content -path $logFile -value ($computerName + " " + $driveLetter + " is gescanned. " + $outFile + " " + (Get-Date).ToString())
-                } else {
-                Add-Content -Path $logFile -Value ("Scannen van " + $computerName + " " + $driveLetter + " is overgeslagen wegens recent log.")
-                }
-            } else {
-                Get-ChildItem -Path $driveLetter -Recurse -Force -ErrorAction SilentlyContinue | Where {$_.Length -gt $fileSize} | Select-Object Mode,Name,CreationTime,DirectoryName,Length | Export-Csv -Path ($outFilePath + $scanFile) -NoTypeInformation
-                Add-Content -path $logFile -value ($computerName + " " + $driveLetter + " is gescanned. " + $outFile + " " + (Get-Date).ToString())
-            }
+	if ($leeftijdBestand.TotalSeconds -gt $leeftijdBestandDagen) {
+	    Remove-Item -Path ($outFilePath + $scanFile) -Force
+	    Add-Content -Path $logFile -Value ("Logboek van " + $computerName + " " + $driveLetter + " is verwijderd wegens " + $leeftijdBestandDagen + " dagen oud.")
+	    Get-ChildItem -Path $driveLetter -Recurse -Force -ErrorAction SilentlyContinue | Where {$_.Length -gt $fileSize} | Select-Object Mode,Name,CreationTime,DirectoryName,Length | Export-Csv -Path ($outFilePath + $scanFile) -NoTypeInformation
+	    Add-Content -path $logFile -value ($computerName + " " + $driveLetter + " is gescanned. " + $outFile + " " + (Get-Date).ToString())
+	} else {
+	    Add-Content -Path $logFile -Value ("Scannen van " + $computerName + " " + $driveLetter + " is overgeslagen wegens recent log.")
+	}
+    } else {
+	Get-ChildItem -Path $driveLetter -Recurse -Force -ErrorAction SilentlyContinue | Where {$_.Length -gt $fileSize} | Select-Object Mode,Name,CreationTime,DirectoryName,Length | Export-Csv -Path ($outFilePath + $scanFile) -NoTypeInformation
+	Add-Content -path $logFile -value ($computerName + " " + $driveLetter + " is gescanned. " + $outFile + " " + (Get-Date).ToString())
+    }
 }
+
 Add-Content -Path $logFile -Value ((Get-Date).ToString() + " Scan afgerond op " + $computerName)
